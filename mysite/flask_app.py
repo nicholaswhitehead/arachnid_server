c4f6585bfd94ab0be7dc6591772d940e601bff5f
Nicholas Whitehead <nicowhitehead@4b5d82362e5e>
2021-11-11 06:29:25 +0000

    Fixed file saving

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import classify as cl
 import os
 
-USER_UPLOADS = 'home/nicowhitehead/arachnid/images'
+USER_UPLOADS = '/home/nicowhitehead/arachnid/images'
 
 app = Flask(__name__)
 app.config['USER_UPLOADS'] = USER_UPLOADS
@@ -25,8 +25,9 @@ def upload_img():
     if request.method=='POST':
         uploaded_image = request.files['file']
         if uploaded_image.filename != '':
-            with open(os.path.join(app.config['USER_UPLOADS'], uploaded_image.filename), "w") as file:
-                file.write(uploaded_image)
+            uploaded_image.save(os.path.join(app.config['USER_UPLOADS'], uploaded_image.filename))
+            # with open(os.path.join(app.config['USER_UPLOADS'], uploaded_image.filename), 'w') as file:
+            #     file.write(uploaded_image)
             return redirect(url_for('index'))
     return render_template('index.html')
 
