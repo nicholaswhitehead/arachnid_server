ad8d31754a0e66b5ed27b5a7235fbe7ab9c00095
nicholaswhitehead <66396821+nicholaswhitehead@users.noreply.github.com>
2021-11-12 13:58:58 -0500

    Update classify.py

--- mysite/classify.py
+++ mysite/classify.py
@@ -1,25 +1,15 @@
 import numpy as np
 import cv2 as cv
 
-# test_image = "r100g200b255.png"
-# img = cv.imread(test_image)
-# cv.imshow("image", img)
-# cv.waitKey()
-
 def most_color(img_file):
     img = cv.imread(img_file)
     img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     height, width, channels = img.shape
-    # print(height, width)
 
-    red = 0
-    green = 0
-    blue = 0
-    for x in range(0,width-1):
-        for y in range(0,height-1):
-            red += img[y][x][0]
-            green += img[y][x][1]
-            blue += img[y][x][2]
+    red = np.sum(img[:,:,0])
+    green = np.sum(img[:,:,1])
+    blue = np.sum(img[:,:,2])
+    
     # print(red/(height*width))
     # print(green/(height*width))
     # print(blue/(height*width))
@@ -35,5 +25,5 @@ def most_color(img_file):
         return "blue"
 
 if __name__=="__main__":
-    test_image = "red.png"
+    test_image = "C:/Users/Nicholas/Documents/GitHub/arachnid/images/green.png"
     print(most_color(test_image))
